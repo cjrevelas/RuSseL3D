2cba2fb4e6d2354237652417fcc686c63b80c312
cjrevelas <cjrevelas@gmail.com>
2021-11-22 14:59:55 +0200

    fix: calculate energy terms with 3d integration
    In the previous version, the energy terms were calculated via
    1d integration.

--- src/adh_ten_alt.f90
+++ src/adh_ten_alt.f90
@@ -1,36 +1,39 @@
-  subroutine adhesion_tension_alternative
-    
-   
-    use xdata
-    use mdata
-   
-   
-    implicit none
+subroutine adhesion_tension_alternative
+!--------------------------------------------------------------------!  
+use xdata
+use mdata
+!--------------------------------------------------------------------!  
+implicit none
+!--------------------------------------------------------------------!
+real(8) :: dterm1(numnp), dterm2(numnp), Q
+!--------------------------------------------------------------------!
+term1 = 0.d00
+term2 = 0.d00
+term3 = 0.d00
+term4 = 0.d00
 
-    real*8 phi_adh(numnp),sum_f,Q
-!---------------------------------------------------------------------
-!**********************************************************************
-!********************Grand potential calculations**********************
-!**********************************************************************
-!**********************************************************************
-!---------------------------------------------------------------------
+do k1 = 1, numnp
+   dterm1(k1) = 0.5d0 * kapa * ((1.d0 - phia_new(k1))**2.d0) !blue: particle-particle interaction
+   dterm2(k1) = -(wa(k1) - Ufield(k1)) * phia_new(k1)        !pink: rho-w interaction
+enddo
 
-         sum_f = 0.d00 
-		 do k1 = 1,numnp
-              phi_adh(k1) = 0.5d0 * kapa * (1.d0-(phia_new(k1))**2.)
-         end do
-         call spat_3d(phi_adh,sum_f,Q)
-!........sum_final is part of Omega N/(rho_0 k_B T a), measured in meters.
-		 sum_f= sum_f * 1.d-10
-         term3=xc(1,numnp)*1.0d-10 *(1.d00 - part_func)
-!DEBUG
-!   term4=-dble(chainlen)/rho_0*log(qf_final(1,ns))
-!DEBUG
-          sum_f = sum_f +term3
+call spat_3d(dterm1, term1, Q)
+call spat_3d(dterm2, term2, Q)
 
-!........Adhesion tension, in mN/m
-!........adh_ten = (gamma_s - gamma_sl)
-          adh_ten_alt = - sum_f* rho_0 * k_B* Temp / dfloat(chainlen) * 1.0D+03
+term1 = term1 * 1.0d-10
+term2 = term2 * 1.0d-10
+term3 = xc(1,numnp) * 1.0d-10 * (1.d00 - part_func)      !red:   translational entropy of free chains
+term4 = -dble(chainlen) / rho_0 * log(qf_final(1,ns))    !green: translational entropy of grafted chains
 
-    return 
-end
+! x1.0D+03 ----> N/m --> mN/m
+part_sum1 = term1 * rho_0 * k_B * Temp / dfloat(chainlen) * 1.0D+03
+part_sum2 = term2 * rho_0 * k_B * Temp / dfloat(chainlen) * 1.0D+03
+part_sum3 = term3 * rho_0 * k_B * Temp / dfloat(chainlen) * 1.0D+03
+part_sum4 = term4 * rho_0 * k_B * Temp / dfloat(chainlen) * 1.0D+03
+
+sum_final   = part_sum1 + part_sum2 + part_sum3 + part_sum4
+adh_ten_alt = - sum_final   ! adh_test = (Omega_bulk - Omega) / A
+
+return
+!--------------------------------------------------------------------!  
+end subroutine adhesion_tension_alternative
