ec006a4684b04a6ed70f9ac45a0da39aaf353e30
cjrevelas <cjrevelas@gmail.com>
2021-11-25 18:16:12 +0200

    refactor(qprint): prevent the printing of -0.0

--- src/qprint.f90
+++ src/qprint.f90
@@ -2,22 +2,40 @@ subroutine qprint
 !--------------------------------------------------------------------!
 use xdata
 use mdata
+use constants
 !--------------------------------------------------------------------!   
 implicit none
 !--------------------------------------------------------------------!
-character(len=20) :: frmt
 integer :: time_step
+real(8) :: iqf_final
 !--------------------------------------------------------------------!	
-write(frmt,'("(3E20.9,",I4,"E20.9)")') ns+1
+
+! APS. Old section
+!character(len=20) :: frmt
+!write(frmt,'("(3E20.9,",I4,"E20.9)")') ns+1
+!open(unit=363, file = 'qfree.out.txt')
+!do i1 = 1, numnp
+!     write (363,frmt) (xc(ii2,i1), ii2 = 1, ndm), (qf_final(i1,time_step), time_step = 1, ns+1)
+!enddo
+!close(363)
+! / Old section
 
 open(unit=363, file = 'qfree.out.txt')
-
 do i1 = 1, numnp
-     write (363,frmt) (xc(ii2,i1), ii2 = 1, ndm), (qf_final(i1,time_step), time_step = 1, ns+1)
+    do ii2 = 1, ndm
+        write(363,'(E20.9)',advance='no') xc(ii2,i1)
+    enddo
+    do time_step = 1, ns+1
+        iqf_final = qf_final(i1,time_step)
+        if (dabs(iqf_final)<tol) then
+            iqf_final = 0.d0
+        endif
+        write(363,'(E20.9)',advance='no') iqf_final
+    enddo
+    write(363,*)
 enddo
-
 close(363)
 
-return 
+return
 !--------------------------------------------------------------------!
 end subroutine qprint 
