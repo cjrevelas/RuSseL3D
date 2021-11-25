324ad68b0910c95aa929f94aa99a06c707ce725c
cjrevelas <cjrevelas@gmail.com>
2021-11-25 14:55:42 +0200

    fix(main): remove integer to real(8) comparison

--- src/main.f90
+++ src/main.f90
@@ -37,7 +37,7 @@ end if
 
 flag_continue = .true.
 !
-! the slaves will enter the mumps subroutine untill they recieve a stop
+! the slaves will enter the mumps subroutine until they receive a stop
 ! signal from master proc
 if (.not.root) then
     do while (.true.)
@@ -52,58 +52,45 @@ if (.not.root) then
 endif
 #endif
 !*******************************************************************!
-!                   GAUSSDAT PARAMETER VALUES                       !
+!                       INITIALIZATION SECTION                      !
 !*******************************************************************!
 ior = 55
 iow = 10
-   
-open(unit=54, file='times.out.txt')
 
-call CPU_TIME(start)  
+! Read and initialize the mesh
 call mesh_io_3d
-call CPU_TIME(t1)
 
-write(6,*)  'numnp', numnp, 'numel', numel
-
-write(54,*) 'numnp', numnp
-write(54,*) 'numel', numel
-write(54, '("Time of mesh_io_3d       : ",I6.3," mins ",F6.3," secs.")') &
-   &        int(t1-start)/60, mod((t1-start),60.)
-
+! Read the gaussdatparameter values
 call scfinout
-!*******************************************************************!
-!                 INITIALIZE SIMPSON COEFFICIENTS                   !
-!                      FOR CONTOUR INTEGRATION                      !
-!*******************************************************************!
+
+! Initialize the Simpson coefficients
 call simpsonkoef_s
 
-open(unit=211, file = 'Usolid.out.txt')
+!*******************************************************************!
+!                       INITIALIZE FIELDS                           !
+!*******************************************************************!
+wa = 0.d0
 
+open(unit=211, file = 'Usolid.out.txt')
 do k1 = 1, numnp
+    !TODO: fix distance for 3D
     distance   = xc(1,k1)
     Ufield(k1) = surf_pot(distance)
     write(211,('(E16.9,2X,E19.9)')) distance, Ufield(k1) 
 enddo
-!*******************************************************************!
-!                       INITIALIZE FIELDS                           !
-!*******************************************************************!
-do i1 = 1, numnp
-     wa(i1) = 0.d0
-enddo
+close(211)
 
 if (show.eq.1) then
-    open(unit=21, file = 'field_in.txt')
-    do i1 = 1, numnp
-        read (21,'(18X,E16.9)') wa(i1)
-        wa(i1)=wa(i1) !*real(chainlen)
-    enddo
+    write(*,*)"Field will be read from a file!"
+    open(unit=21, file = 'field.inout.bin', Form='unformatted')
+    read(21) wa
     close(21)
 endif
 !*******************************************************************!
 !                       LOOPS FOR SOLUTION                          !
 !*******************************************************************!
 write(6,*) 'Iteration,  Adh. tension (mN/m),  error(beta N w)'
-write(iow,'(A10,2X,A16,2X,A16,2X,A16)') 'kk', 'adh_ten', 'error'
+write(iow,'(A10,2X,A16,2X,A16)') 'kk', 'adh_ten', 'error'
 
 kk=0
 error=200000.
@@ -111,49 +98,49 @@ error=200000.
 do while ((kk.lt.iterations).and.(error.gt.max_error))
     kk=kk+1
 
-    call CPU_TIME(t2)
     call matrix_assemble
-    call CPU_TIME(t3)
 
-    write(54, '("Time of matrix_assemble  : ",I6.3," mins ",F6.3," secs.")') int(t3-t2)/60, mod((t3-t2),60.)
-
+    ! Solve the diffusion equation
     call edwards_free_film_fem
-    call CPU_TIME(t4)
 
-    write(54, '("Time of Edwards solution : ",I6.3,"minutes",F6.3," seconds.")') int(t4-t3)/60, mod((t4-t3),60.)
-    write(54, '(A13,e16.9,2x,e16.9,2x,e16.9,2x,e16.9,2x,e16.9)') "start, t1-4: ",start, t1, t2, t3, t4
-
-    close(54)
-
-    if (mod(kk,10000).eq.0) then
-        open(unit=21, file = 'field.out.txt')
-        do k1 = 1, numnp
-            write(21,'(E16.9,2X,E16.9)') xc(1,k1), wa(k1)
-        enddo
-        close(21)
-    endif
-
+    ! Calculate the partition function and reduced density
     call part_fun_phi
+
+    ! Calculate the new field
+    do k1 = 1, numnp
+        !APS CHECK xc(1 or xc(3
+        !distance   = xc(1,k1)
+        wa_new(k1) = kapa * (phia_new(k1)- 1.d0) + Ufield(k1)
+    enddo
+
+    ! Calculate adhesion tension
+    call adhesion_tension
+
     !*******************************************************************!
     !                     PERIODIC EXPORT OF PROFILES                   !
     !*******************************************************************!
-    open (unit=120, file = 'reduced_density_profiles.out.txt')
-    !if (mod(kk,1000).eq.0.d0) then
-        write(120,'(A16,2X,A16)') 'z','phi(z)'
+    if (mod(kk,1).eq.0) then
+!APS TEMP
+        open (unit=120, file = 'rho_reduced.out.txt')
+        write(120,'(8(A17))') 'np','x','y','z','phi','wa_old','wa_new','wa_mix'
         do k1 = 1, numnp
-            write(120,'(E16.9,2X,E16.9,2X,E16.9,2X,E16.9)') xc(1,k1), xc(2,k1), xc(3,k1), phia_new(k1)
+            write(120,'(I17,7(E17.9))') k1, xc(1,k1), xc(2,k1), xc(3,k1), phia_new(k1), &
+   &                     wa(k1), wa_new(k1), (1.d0-fraction) * wa(k1) + fraction * wa_new(k1)
         enddo
-    !endif
-    close(120)
 
-    call adhesion_tension
-    !*******************************************************************!
-    !                        CALCULATE NEW FIELD                        !
-    !*******************************************************************!
-    do k1 = 1, numnp
-        distance   = xc(1,k1)
-        wa_new(k1) = kapa * (phia_new(k1)- 1.d0) + Ufield(k1)
-    enddo
+!APS TEMP SECTION
+!write(frmt,'("(3E20.9,",I4,"E20.9)")') ns+1
+        open (unit=120, file = 'rho.out.txt', status='unknown', position='append')
+!           write(120,'(14374(E17.9))') (xc(3,k1*2), k1=1,numnp/2)
+           write(120,'(14374(E17.9))') (phia_new(k1*2), k1=1,numnp/2)
+        close(120)
+!/APS TEMP SECTION
+    endif
+
+    if (pr_on==1) then
+        call qprint
+    endif
+
     !*******************************************************************!
     !                     APPLY CONVERGENCE CRITERION                   !
     !*******************************************************************!
@@ -168,23 +155,17 @@ do while ((kk.lt.iterations).and.(error.gt.max_error))
         wa(k1) = (1.d0-fraction) * wa(k1) + fraction * wa_new(k1)
     enddo
 
-    if (mod(kk,1000).eq.0d0) then
-        open(unit=123, file = 'field_compare.txt')
-        do k1 = 1, numnp
-            write(123,'(I10,2X,E16.9,2X,E16.9)') k1, wa(k1), wa_new(k1)
-        enddo
+    if (mod(kk,1).eq.0) then
+        write(6,*)  kk, adh_ten, error
+        write(iow,'(I10,2X,E16.9,2X,E16.9)') kk, adh_ten, error
     endif
 
-    if (mod(kk,100).eq.0.d0) then
-        write(6,*)  kk, adh_ten_alt, error
-        write(iow,'(I10,2X,E16.9,2X,E16.9)') kk, adh_ten_alt, error
-    endif
+    ! Print the field for restart
+    open(unit=21, file = 'field.inout.bin', Form='unformatted')
+    write(21) wa
+    close(21)
 
 enddo!kk
-
-if (pr_on==1) then
-    call qprint
-endif
 
 if (error.lt.max_error) then
     write(iow,'(/''Convergence of max error'',F16.9)') error
@@ -193,11 +174,17 @@ else
 endif
 
 write(iow,'(''-----------------------------------'')')
-write(iow,'(''Adhesion tension (mN/m) '',E16.9)') adh_ten_alt
+write(iow,'(''Adhesion tension (mN/m) '',E16.9)') adh_ten
 write(iow,'(''Partition function Q =  '',E16.9)') part_func
 write(iow,'(''            n/n_bulk =  '',E16.9)') nch_per_area * dfloat(chainlen) / (rho_0 * lx  * 1.d-10)
 
 #ifdef USE_MPI
+! Root will send a stop signal to the slaves
+if (root) then
+    flag_continue = .false.
+    call MPI_BCAST(flag_continue, 1, MPI_LOGICAL, 0, MPI_COMM_WORLD, ierr)
+end if
+
 1000 CALL MPI_FINALIZE(ierr)
 #endif
 !------------------------------------------------------------------------------------------------------------------!
