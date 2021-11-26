c4507444f2d9b61c150fc218f457750ad292fbe7
cjrevelas <cjrevelas@gmail.com>
2021-11-26 22:05:16 +0200

    feat: export field in ascii and binary files

--- src/main.f90
+++ src/main.f90
@@ -92,7 +92,7 @@ do k1 = 1, numnp
     !TODO: fix distance for 3D
     distance   = xc(1,k1)
     Ufield(k1) = surf_pot(distance)
-    write(211,('(E16.9,2X,E19.9)')) distance, Ufield(k1) 
+    write(211,('(E16.9,2X,E19.9)')) distance, Ufield(k1)
 enddo
 close(211)
 
@@ -136,23 +136,32 @@ do while ((kk.lt.iterations).and.(error.gt.max_error))
     !*******************************************************************!
     !                     PERIODIC EXPORT OF PROFILES                   !
     !*******************************************************************!
-    if (mod(kk,1).eq.0) then
-!APS TEMP
-        open (unit=120, file = 'rho_reduced.out.txt')
-        write(120,'(8(A13))') 'np','x','y','z','phi','wa_old','wa_new','wa_mix'
-        do k1 = 1, numnp
-            write(120,'(I13,7(E13.5))') k1, xc(1,k1), xc(2,k1), xc(3,k1), phia_new(k1), &
-   &                     wa(k1), wa_new(k1), (1.d0-fraction) * wa(k1) + fraction * wa_new(k1)
-        enddo
 
-!APS TEMP SECTION
-!write(frmt,'("(3E20.9,",I4,"E20.9)")') ns+1
-        open (unit=120, file = 'rho.out.txt', status='unknown', position='append')
-!           write(120,'(14374(E17.9))') (xc(3,k1*2), k1=1,numnp/2)
-           write(120,'(14374(E17.9))') (phia_new(k1*2), k1=1,numnp/2)
+!APS TEMP Generate the headers of files
+    print_ev = 3
+    if (zero_field.and.iter.eq.1) then
+        open (unit=120, file = 'rho.out.txt')
+        do k1 = 1, numnp, print_ev
+            write(120,'(E13.5)',advance='no') xc(3,k1)
+        enddo
         close(120)
-!/APS TEMP SECTION
+        open (unit=120, file = 'wa.out.txt')
+        do k1 = 1, numnp, print_ev
+            write(120,'(E13.5)',advance='no') xc(3,k1)
+        enddo
+        close(120)
+        open (unit=120, file = 'wa_new.out.txt')
+        do k1 = 1, numnp, print_ev
+            write(120,'(E13.5)',advance='no') xc(3,k1)
+        enddo
+        close(120)
+        open (unit=120, file = 'wa_mix.out.txt')
+        do k1 = 1, numnp, print_ev
+            write(120,'(E13.5)',advance='no') xc(3,k1)
+        enddo
+        close(120)
     endif
+!/APS TEMP SECTION
 
     if (pr_on==1) then
         call qprint
@@ -163,33 +172,93 @@ do while ((kk.lt.iterations).and.(error.gt.max_error))
     !*******************************************************************!
     error = 0.d00
     do k1 = 1, numnp
-        error = max(error, dabs(wa_new(k1) - wa(k1)))
+        error = error + (dabs(wa_new(k1) - wa(k1)))
     enddo
+    error = error / numnp
     !*******************************************************************!
-    !                     APPLY ANDERSON MIXING RULE                    !
+    !                         APPLY MIXING RULE                         !
     !*******************************************************************!
+
+    wa_max=0.d0
+    ! APS TEMP dabs(wa_new) might not work properly for wa_max < kapa!!!
     do k1 = 1, numnp
-        wa(k1) = (1.d0-fraction) * wa(k1) + fraction * wa_new(k1)
+        wa_max = max(wa_max, dabs(wa_new(k1)))
     enddo
 
-    !CJR: Added fraction for mixing
-    if (fraction > 1.d0 / (10.d0*kapa)) then
-        fraction = 1.d0 / (10.d0*kapa)
-    else
-        fraction = mix_coef * fraction
+    !   original convergence scheme
+    if (scheme_type.eq.1) then
+        if (.not.zero_field) then
+            fraction = min(fraction * mix_coef_frac, 1.d0 / kapa / mix_coef_kapa)
+        endif
+        if (zero_field.and.iter.eq.1) then
+            fraction = 1.d0
+        endif
+        if (zero_field.and.iter.eq.2) then
+            fraction = 1.d0 / (wa_max * 100.d0)
+            zero_field = .false.
+        endif
     endif
 
-    if (mod(kk,1).eq.0) then
-        write(6,*)  kk, adh_ten, error, fraction
-        write(iow,'(I10,2X,E16.9,2X,E16.9,2X,E16.9)') kk, adh_ten, error, fraction
+    ! experimental scheme
+    if (scheme_type.eq.2) then
+        fraction = min(1.d0 / wa_max / mix_coef_frac, 1.d0 / kapa / mix_coef_kapa)
+
+        if (zero_field) then
+            fraction = 1.d0
+            zero_field = .false.
+        endif
     endif
+
+    do k1 = 1, numnp
+        wa_mix(k1) = (1.d0-fraction) * wa(k1) + fraction * wa_new(k1)
+    enddo
+
+    write(6  ,'(I10,2X,E16.9,2X,E16.9,2X,E16.9,2X,E16.9)') iter, adh_ten, error, wa_max, fraction
+    write(iow,'(I10,2X,E16.9,2X,E16.9,2X,E16.9,2X,E16.9)') iter, adh_ten, error, wa_max, fraction
+
+
+    if (mod(iter,1).eq.0) then
+        open (unit=121, file = 'wa.out.txt', status='unknown', position='append')
+        open (unit=122, file = 'wa_new.out.txt', status='unknown', position='append')
+        open (unit=123, file = 'wa_mix.out.txt', status='unknown', position='append')
+        open (unit=124, file = 'rho.out.txt', status='unknown', position='append')
+        do k1 = 1, numnp, print_ev
+            write(121,'(E13.5)',advance='no') wa(k1)
+            write(122,'(E13.5)',advance='no') wa_new(k1)
+            write(123,'(E13.5)',advance='no') wa_mix(k1)
+            write(124,'(E13.5)',advance='no') phia_new(k1)
+        enddo
+        write(121,*)
+        write(122,*)
+        write(123,*)
+        write(124,*)
+        close(121)
+        close(122)
+        close(123)
+        close(124)
+
+        open (unit=120, file = 'rho_reduced.out.txt')
+        write(120,'(8(A13))') 'np','x','y','z','phi','wa','wa_new','wa_mix'
+        do k1 = 1, numnp
+            write(120,'(I13,7(E13.5))') k1, xc(1,k1), xc(2,k1), xc(3,k1), phia_new(k1), &
+   &                                    wa(k1), wa_new(k1), wa_mix(k1)
+        enddo
+        close(120)
+    endif
+
+    wa = wa_mix
 
     ! Print the field for restart
-    open(unit=21, file = 'field.inout.bin', Form='unformatted')
-    write(21) wa
+#ifdef REDUCE_W_CHLEN
+    do k1 = 1, numnp
+        wa_mix(k1) = wa_mix(k1) / chainlen
+    enddo
+#endif
+    open(unit=21, file = 'field.out.bin', Form='unformatted')
+    write(21) wa_mix
     close(21)
 
-enddo!kk
+enddo!iter
 
 if (error.lt.max_error) then
     write(iow,'(/''Convergence of max error'',F16.9)') error
@@ -209,7 +278,7 @@ if (root) then
     call MPI_BCAST(flag_continue, 1, MPI_LOGICAL, 0, MPI_COMM_WORLD, ierr)
 end if
 
-1000 CALL MPI_FINALIZE(ierr)
+1000 call MPI_FINALIZE(ierr)
 #endif
 write(*,*)"Done!"
 !------------------------------------------------------------------------------------------------------------------!
