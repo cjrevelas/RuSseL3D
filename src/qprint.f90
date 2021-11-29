295a4e6c21de1f9d068e8a5659a8c1aa797e9953
cjrevelas <cjrevelas@gmail.com>
2021-11-29 22:28:21 +0200

    refactor: export propagators across lines

--- src/qprint.f90
+++ src/qprint.f90
@@ -9,11 +9,18 @@ integer :: time_step, i1, ii2
 
 character(4)                               :: q_type
 character(20)                              :: file_name
+
 real(8), intent(in), dimension(numnp,ns+1) :: q_final
 real(8)                                    :: iq_final
-!--------------------------------------------------------------------!
 
-write(file_name,'(''q'',A4,''.out.txt'')')q_type
+! Profile Section
+real(8), parameter                         :: rtol = 2.d0
+integer, parameter                         :: max_time_step = 40
+real(8), dimension(3)                      :: r_center
+integer                                    :: d2, d3
+
+!--------------------------------------------------------------------!
+write(file_name,'(''q'',A4,''.out.txt'')') q_type
 
 open(unit=363, file = file_name)
 do i1 = 1, numnp
@@ -31,6 +38,41 @@ do i1 = 1, numnp
 enddo
 close(363)
 
+!--------------------------------------------------------------------!
+! The current section prints q's across the three perpendicular lines
+! crossing the point r_center
+r_center(1) = 0.d0
+r_center(2) = 0.d0
+r_center(3) = 0.d0
+
+file_name = ""
+write(file_name,'(''q'',A4,''_prof.out.txt'')') q_type
+open(unit=363, file = file_name)
+
+do ii2 = 1, ndm
+    write(363,*) "dim ", ii2
+    write(363,*) "pos times..."
+    write(363,*)
+
+    d2 = mod(ii2+0,3)+1
+    d3 = mod(ii2+1,3)+1
+
+    do i1 = 1, numnp
+        if ( dabs(xc(d2,i1) - r_center(d2)) < rtol .and.  &
+   &         dabs(xc(d3,i1) - r_center(d3)) < rtol ) then
+            write(363,'(1(E20.9))',advance='no') xc(ii2,i1)
+            !do time_step = 1, max_time_step
+            do time_step = 1, ns+1
+                write(363,'(E20.9)',advance='no') q_final(i1,time_step)
+            enddo
+            write(363,*)
+        endif
+    enddo
+    write(363,*)
+enddo
+
+close(363)
+
 return
 !--------------------------------------------------------------------!
-end subroutine qprint 
+end subroutine qprint
