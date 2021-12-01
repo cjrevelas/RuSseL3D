b2ecbbc004c4d062b791362ec53a98b9e1f95d1d
cjrevelas <cjrevelas@gmail.com>
2021-12-01 21:19:15 +0200

    refactor: rename nch_gr_per_area to nch_gr

--- src/grafted_chains.f90
+++ src/grafted_chains.f90
@@ -1,4 +1,4 @@
-subroutine grafted_chains(numnp, chainlen, rho_0, phia, nch_per_area)
+subroutine grafted_chains(numnp, chainlen, rho_0, phia, nch_gr)
 !-------------------------------------------------------------------------------------------!
 use constants
 !-------------------------------------------------------------------------------------------!
@@ -8,14 +8,14 @@ integer, intent(in)                        :: numnp
 
 real(8), intent(in)                        :: chainlen, rho_0
 real(8), intent(in), dimension(numnp)      :: phia
-real(8), intent(out)                       :: nch_per_area
+real(8), intent(out)                       :: nch_gr
 real(8)                                    :: sum_f, Q
 !-------------------------------------------------------------------------------------------!
 sum_f = 0.d0
 
 call spat_3d(phia, sum_f, Q)
 
-nch_per_area = sum_f*1.0d-30*rho_0*avogadro_constant/chainlen
+nch_gr = sum_f*1.0d-30*rho_0*avogadro_constant/chainlen
 
 return
 !-------------------------------------------------------------------------------------------!
