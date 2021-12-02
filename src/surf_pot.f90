bca67591d97fe23e607fe70e789f18a5cb34aa59
cjrevelas <cjrevelas@gmail.com>
2021-12-02 19:14:49 +0200

    refactor(surf_pot): make it a sub and purify from parser_vars

--- src/surf_pot.f90
+++ src/surf_pot.f90
@@ -1,23 +1,20 @@
-function surf_pot(h12)
-!--------------------------------------------------------------------!
-use parser_vars
+subroutine surf_pot(Temp, h12, sphere_radius, rho_0, sigma1, sigma2, Aps, Asio2, r12, Urep, Uatt, Utot)
+!------------------------------------------------------------------------------------------------------!
+use error_handing
 use constants
-!--------------------------------------------------------------------!
+!------------------------------------------------------------------------------------------------------!
 implicit none
-!--------------------------------------------------------------------!
-integer :: simple
-
-real(8), intent(in) :: h12
-real(8)             :: surf_pot
-real(8)             :: sigma
-real(8)             :: Urep, Uattr
-real(8)             :: a1, a2, r12, h_12
-real(8)             :: hamaker_constant
-real(8)             :: number_density
-!--------------------------------------------------------------------!
-simple = 0
-
-open(unit=43, file = 'Hamaker_potential.out.txt')
+!------------------------------------------------------------------------------------------------------!
+real(8), intent(in)  :: Temp, h12, sphere_radius, rho_0, sigma1, sigma2, Aps, Asio2
+real(8), intent(out) :: r12, Urep, Uatt, Utot
+real(8)              :: sigma, hamaker_constant, number_density
+real(8)              :: a1, a2, h_12
+!------------------------------------------------------------------------------------------------------!
+!***SI units are used everywhere***
+r12  = 0.d0
+Urep = 0.d0
+Uatt = 0.d0
+Utot = 0.d0
 
 hamaker_constant = sqrt(Aps*Asio2)
 
@@ -27,56 +24,35 @@ h_12 = h12/1.e+10
 
 number_density = rho_0*avogadro_constant
 
-a1 = (3./4./pi/number_density)**(1./3.)
-a2 = sphere_radius/1.e+10
-
+a1  = (3./4./pi/number_density)**(1./3.)
+a2  = sphere_radius/1.e+10
 r12 = a1 + a2 + h_12
 
-surf_pot = 0.d0
+if (h12.gt.1.e-10) then
+    Uatt = -hamaker_constant/(6.d00) * (                            &
+           (2*a1*a2)/(r12**2.-(a1+a2)**2.) +                    &
+           (2*a1*a2)/(r12**2.-(a1-a2)**2.) +                    &
+            log((r12**2.-(a1+a2)**2.)/(r12**2.-(a1-a2)**2.))  )
 
-if (simple.eq.0)then
-    if (h12.gt.1.e-30) then
+    Urep = hamaker_constant*sigma**6./(37800.*r12)*  (                               &
+           ( r12**2.-7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12-a1-a2)**7.  &
+          +( r12**2.+7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12+a1+a2)**7.  &
+          -( r12**2.+7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12+a1-a2)**7.  &
+          -( r12**2.-7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12-a1+a2)**7.  )
 
-        Uattr = - hamaker_constant/(6.d00) * (         &
-                  (2*a1*a2)/(r12**2.-(a1+a2)**2.) +    &
-                  (2*a1*a2)/(r12**2.-(a1-a2)**2.) +    &
-                  log((r12**2.-(a1+a2)**2.)/(r12**2.-(a1-a2)**2.))  )
-
-
-        Urep = hamaker_constant*sigma**6./(37800.*r12)*                                 &
-                  (                                                                     &
-                  ( r12**2.-7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12-a1-a2)**7.  &
-                 +( r12**2.+7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12+a1+a2)**7.  &
-                 -( r12**2.+7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12+a1-a2)**7.  &
-                 -( r12**2.-7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12-a1+a2)**7.  )
-
-        surf_pot = Uattr + Urep
-    endif
-else
-    if (h12.gt.1.e-30) then
-
-        Uattr = - hamaker_constant/(6.d00*pi*h_12*h_12*h_12*number_density)
-
-        Urep  = hamaker_constant*sigma**6. &
-              /(45.d00*pi*number_density*h_12**9.)
-
-        surf_pot = Uattr + Urep
-    endif
+    Utot = Uatt + Urep
 endif
 
-Uattr    = Uattr/(boltz_const_Joule_K*Temp)
-Urep     = Urep/(boltz_const_Joule_K*Temp)
-surf_pot = surf_pot/(boltz_const_Joule_K*Temp)
+Uatt = Uatt/(boltz_const_Joule_K*Temp)
+Urep = Urep/(boltz_const_Joule_K*Temp)
+Utot = Utot/(boltz_const_Joule_K*Temp)
 
-write(43,'(E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3)') h12, r12, Uattr, Urep, surf_pot
-surf_pot = surf_pot
-
-
-
-if (surf_pot.gt.0.) then
-    surf_pot = 0.d0
+if (Utot.gt.0.) then
+    Utot = 0.d0
 endif
+
+r12 = r12 * 1.e+10
 
 return
-!--------------------------------------------------------------------!
-end function surf_pot
+!------------------------------------------------------------------------------------------------------!
+end subroutine surf_pot
