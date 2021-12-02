5b7d456f0f0442090b42bde016f44fe9c322e662
cjrevelas <cjrevelas@gmail.com>
2021-12-02 19:39:13 +0200

    fix(adh_ten): remove the doubling of interf_area

--- src/adh_ten.f90
+++ src/adh_ten.f90
@@ -24,8 +24,8 @@ term3 = 0.d00
 term4 = 0.d00
 
 do k1 = 1, numnp
-   dterm1(k1) = 0.5d0 * kapa * ((1.d0 - phia_fr(k1) - phia_gr(k1))**2.d0) * chainlen_free !blue: particle-particle interaction ! TEMP NEW
-   dterm2(k1) = -(wa(k1) - Ufield(k1)) * (phia_fr(k1) + phia_gr(k1)) * chainlen_free      !pink: rho-w interaction ! TEMP NEW
+   dterm1(k1) = 0.5d0 * kapa * ((1.d0 - phia_fr(k1) - phia_gr(k1))**2.d0) * chainlen_free !blue: particle-particle interaction
+   dterm2(k1) = -(wa(k1) - Ufield(k1)) * (phia_fr(k1) + phia_gr(k1)) * chainlen_free      !pink: rho-w interaction
 enddo
 
 call spat_3d(dterm1, term1, Q)
@@ -33,7 +33,7 @@ call spat_3d(dterm2, term2, Q)
 
 term1 = term1 * 1.0d-30
 term2 = term2 * 1.0d-30
-term3 = volume * 1.0d-30 * (1.d00 - part_func)          !red:   translational entropy of free chains
+term3 = volume * 1.0d-30 * (1.d00 - part_func) !red: translational entropy of free chains
 
 term4 = 0.d0
 do k1 = 1, numnp
@@ -41,7 +41,7 @@ do k1 = 1, numnp
         term4 = term4 + log(qf_final(k1,ns_gr+1))
     endif
 enddo
-term4 = -chainlen_free / rho_0 * term4                    !green: translational entropy of grafted chains
+term4 = -chainlen_free / rho_0 * term4 !green: translational entropy of grafted chains
 
 ! x1.0D+03 ----> N/m --> mN/m
 part_sum1 = term1 * rho_0 * boltz_const_Joule_molK * Temp / chainlen_free * 1.0D+03
@@ -51,7 +51,7 @@ part_sum4 = term4 * rho_0 * boltz_const_Joule_molK * Temp / chainlen_free * 1.0D
 
 sum_final = part_sum1 + part_sum2 + part_sum3! + part_sum4
 
-adh_ten = - sum_final/(2*interf_area*1.D-20)   ! adh_ten = (Omega_bulk - Omega) / A
+adh_ten = - sum_final/(interf_area*1.D-20) ! adh_ten = (Omega_bulk - Omega) / A
 
 return
 !-------------------------------------------------------------------------------------------------!
