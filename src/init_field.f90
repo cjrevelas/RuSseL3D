cf80383f0856056ac6654cd6ad445186ff88613f
cjrevelas <cjrevelas@gmail.com>
2021-12-06 17:24:20 +0200

    fix(init_field): determine hss by removing wall_dist

--- src/init_field.f90
+++ src/init_field.f90
@@ -13,7 +13,9 @@ implicit none
 integer :: k1, m1, n1
 
 real(8), intent(out), dimension(numnp) :: Ufield, wa
-real(8)                                :: number_density = 0.d0, distance = 0.d0, radius_pol = 0.d0
+real(8)                                :: number_density = 0.d0
+real(8)                                :: r_centers = 0.d0, r_center_surf = 0.d0, r_surf = 0.d0
+real(8)                                :: radius_np_actual = 0.d0, radius_pol = 0.d0
 real(8)                                :: Urep = 0.d0, Uatt = 0.d0
 !------------------------------------------------------------------------------------------------------!
 open(unit=211, file = usolid)
@@ -27,71 +29,63 @@ do k1 = 1, numnp
             if (is_dir_face(m1,n1)) then
 
                 number_density = rho_0 * avogadro_constant
-                radius_pol     = (3./4./pi/number_density)**(1./3.)
-                wall_distance  = radius_pol
+                radius_pol     = (3./4./pi/number_density)**(1./3.) * 1.d10
 
                 if (n1.eq.1) then
-                    distance = xc(m1,k1) - box_lo(m1) - radius_pol + wall_distance
+                    r_center_surf = xc(m1,k1) - box_lo(m1) + wall_distance
                 elseif (n1.eq.2) then
-                    distance = box_hi(m1) - xc(m1,k1) - radius_pol + wall_distance
+                    r_center_surf = box_hi(m1) - xc(m1,k1) + wall_distance
                 endif
 
-                call hamaker_sphere_plate(distance, radius_pol, sigma_pol, sigma_plate(1), A_pol, A_plate(1), Urep, Uatt)
+                r_surf = r_center_surf - radius_pol
+
+                call hamaker_sphere_plate(r_surf, radius_pol, sigma_pol, sigma_plate(1), A_pol, A_plate(1), Urep, Uatt)
 
                 Urep = Urep/(boltz_const_Joule_K*Temp)
                 Uatt = Uatt/(boltz_const_Joule_K*Temp)
-
-                if ((Urep+Uatt).gt.0.) then
-                    Urep = 0.d0
-                    Uatt = 0.d0
-                endif
 
                 Ufield(k1) = Ufield(k1) + Urep + Uatt
 
                 if (Ufield(k1).ne.Ufield(k1)) then
                     write(ERROR_MESSAGE,'("Hamaker assumed a NaN value for x = ",E16.9,". &
-                                        & NaN was changed to ",E16.9)') distance, Ufield(k1)
+                                        & NaN was changed to ",E16.9)') r_surf, Ufield(k1)
                     call exit_with_error(1,2,1,ERROR_MESSAGE)
                 endif
 
-                if (distance.lt.0.d0) then
-                    write(ERROR_MESSAGE,'("Hamaker distance smaller than zero! (",E16.9,").")') distance
+                if (r_surf.lt.0.d0) then
+                    write(ERROR_MESSAGE,'("Hamaker distance smaller than zero! (",E16.9,").")') r_surf
                     call exit_with_error(1,2,1,ERROR_MESSAGE)
                 endif
 
-                write(211,'(E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3)') distance, Uatt, Urep, Urep + Uatt
+                write(211,'(E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3)') r_center_surf, r_surf, Uatt, Urep, Urep + Uatt
             endif
         enddo
     enddo
 
    !loop over all nanoparticle faces
+   number_density   = rho_0 * avogadro_constant
+   radius_pol       = (3./4./pi/number_density)**(1./3.) * 1.d10
+
    do m1 = 1, n_nanopart_faces
 
-       number_density = rho_0 * avogadro_constant
-       radius_pol     = (3./4./pi/number_density)**(1./3.)
-       radius_np(m1)  = radius_np(m1)/1.e+10
-       distance       = dsqrt((xc(1,k1)-center_np(1,m1))**2 + (xc(2,k1)-center_np(2,m1))**2 + (xc(3,k1)-center_np(3,m1))**2) &
-                   &  - radius_pol - radius_np(m1)
+       r_centers        = dsqrt((xc(1,k1)-center_np(1,m1))**2 + (xc(2,k1)-center_np(2,m1))**2 + (xc(3,k1)-center_np(3,m1))**2)
+       radius_np_actual = radius_np(m1) - wall_distance
+       r_surf           = r_centers - radius_pol - radius_np_actual
 
-       call hamaker_sphere_sphere(distance, radius_pol, radius_np(m1), sigma_pol, sigma_np(m1), A_pol, A_np(m1), Urep, Uatt)
+       call hamaker_sphere_sphere(r_surf, radius_pol, radius_np_actual, sigma_pol, sigma_np(m1), A_pol, A_np(m1), Urep, Uatt)
 
        Urep = Urep/(boltz_const_Joule_K*Temp)
        Uatt = Uatt/(boltz_const_Joule_K*Temp)
-
-       if ((Urep+Uatt).gt.0.) then
-           Urep = 0.d0
-           Uatt = 0.d0
-       endif
 
        Ufield(k1) = Ufield(k1) + Urep + Uatt
 
        if (Ufield(k1).ne.Ufield(k1)) then
            write(ERROR_MESSAGE,'("Hamaker assumed a NaN value for x = ",E16.9,". &
-                                & NaN was changed to ",E16.9)') distance, Ufield(k1)
+                                & NaN was changed to ",E16.9)') r_surf, Ufield(k1)
            call exit_with_error(1,2,1,ERROR_MESSAGE)
        endif
 
-       write(211,'(E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3)') distance, Uatt, Urep, Urep + Uatt
+       write(211,'(E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3,2X,E17.9E3)')  r_centers, r_surf, Uatt, Urep, Urep + Uatt
    enddo
 enddo
 
