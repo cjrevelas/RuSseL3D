ca9fd5edd7741dc431145799c670fdd47971310e
cjrevelas <cjrevelas@gmail.com>
2021-12-06 16:05:23 +0200

    feat(force_fields): add sphere-plate Hamaker potential

--- src/mod/force_fields.f90
+++ src/mod/force_fields.f90
@@ -4,64 +4,30 @@ implicit none
 !--------------------------------------------------------------------!
 contains
 
-subroutine hamaker_sphere_plate(Temp, h12, rho_0, sigma1, sigma2, Aps, Asio2, r12, Urep, Uatt, Utot)
-!------------------------------------------------------------------------------------------------------!
-use error_handing
-use constants
+subroutine hamaker_sphere_plate(h12, r_pol, sigma1, sigma2, A1, A2, Urep, Uatt)
 !------------------------------------------------------------------------------------------------------!
 implicit none
 !------------------------------------------------------------------------------------------------------!
-real(8), intent(in)  :: Temp, h12, rho_0, sigma1, sigma2, Aps, Asio2
-real(8), intent(out) :: r12, Urep, Uatt, Utot
-real(8)              :: sigma, hamaker_constant, number_density
-real(8)              :: a1, a2, h_12
-real(8)              :: sphere_radius_TEMP
+real(8), intent(in)  :: h12, r_pol, sigma1, sigma2, A1, A2
+real(8), intent(out) :: Urep, Uatt
+real(8)              :: sigma = 0.d0, hamaker_constant = 0.d0, h_12 = 0.d0
 !------------------------------------------------------------------------------------------------------!
 !***SI units are used everywhere***
-
-sphere_radius_TEMP = 80.d0
-
-r12  = 0.d0
 Urep = 0.d0
 Uatt = 0.d0
-Utot = 0.d0
 
-hamaker_constant = sqrt(Aps*Asio2)
+hamaker_constant = sqrt(A1*A2)
 
 sigma = (sigma1 + sigma2)/2. * 1.e-10
 
 h_12 = h12/1.e+10
 
-number_density = rho_0*avogadro_constant
-
-a1  = (3./4./pi/number_density)**(1./3.)
-a2  = sphere_radius_TEMP/1.e+10
-r12 = a1 + a2 + h_12
-
 if (h12.gt.1.e-10) then
-    Uatt = -hamaker_constant/(6.d00) * (                            &
-           (2*a1*a2)/(r12**2.-(a1+a2)**2.) +                    &
-           (2*a1*a2)/(r12**2.-(a1-a2)**2.) +                    &
-            log((r12**2.-(a1+a2)**2.)/(r12**2.-(a1-a2)**2.))  )
+    Uatt = - (hamaker_constant/6.d0) * (1.d0/(h_12/r_pol) + 1.d0/(2.d0+h_12/r_pol) + log((h_12/r_pol)/(2.d0+h_12/r_pol)))
 
-    Urep = hamaker_constant*sigma**6./(37800.*r12)*  (                               &
-           ( r12**2.-7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12-a1-a2)**7.  &
-          +( r12**2.+7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12+a1+a2)**7.  &
-          -( r12**2.+7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12+a1-a2)**7.  &
-          -( r12**2.-7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12-a1+a2)**7.  )
-
-    Utot = Uatt + Urep
+    Urep = (hamaker_constant/7.56d3)*(sigma/r_pol)**6.d0*((8.d0 + h_12/r_pol)/(2.d0 + h_12/r_pol)**7.d0 &
+                                                           & + (6.d0 - h_12/r_pol)/(h_12/r_pol)**7.d0)
 endif
-
-Uatt = Uatt/(boltz_const_Joule_K*Temp)
-Urep = Urep/(boltz_const_Joule_K*Temp)
-Utot = Utot/(boltz_const_Joule_K*Temp)
-
-if (Utot.gt.0.) then
-    Utot = 0.d0
-endif
-
-r12 = r12 * 1.e+10
 
 return
 !------------------------------------------------------------------------------------------------------!
@@ -69,60 +35,38 @@ end subroutine hamaker_sphere_plate
 
 
 
-subroutine hamaker_sphere_sphere(Temp, h12, sphere_radius, rho_0, sigma1, sigma2, Aps, Asio2, r12, Urep, Uatt, Utot)
-!------------------------------------------------------------------------------------------------------!
-use error_handing
-use constants
+subroutine hamaker_sphere_sphere(h12, r1, r2, sigma1, sigma2, A1, A2, Urep, Uatt)
 !------------------------------------------------------------------------------------------------------!
 implicit none
 !------------------------------------------------------------------------------------------------------!
-real(8), intent(in)  :: Temp, h12, sphere_radius, rho_0, sigma1, sigma2, Aps, Asio2
-real(8), intent(out) :: r12, Urep, Uatt, Utot
-real(8)              :: sigma, hamaker_constant, number_density
-real(8)              :: a1, a2, h_12
+real(8), intent(in)  :: h12, r1, r2, sigma1, sigma2, A1, A2
+real(8), intent(out) :: Urep, Uatt
+real(8)              :: r12 = 0.d0, sigma = 0.d0, hamaker_constant = 0.d0, h_12 = 0.d0
 !------------------------------------------------------------------------------------------------------!
 !***SI units are used everywhere***
-r12  = 0.d0
 Urep = 0.d0
 Uatt = 0.d0
-Utot = 0.d0
 
-hamaker_constant = sqrt(Aps*Asio2)
+hamaker_constant = sqrt(A1*A2)
 
 sigma = (sigma1 + sigma2)/2. * 1.e-10
 
 h_12 = h12/1.e+10
 
-number_density = rho_0*avogadro_constant
-
-a1  = (3./4./pi/number_density)**(1./3.)
-a2  = sphere_radius/1.e+10
-r12 = a1 + a2 + h_12
+r12 = r1 + r2 + h_12
 
 if (h12.gt.1.e-10) then
-    Uatt = -hamaker_constant/(6.d00) * (                            &
-           (2*a1*a2)/(r12**2.-(a1+a2)**2.) +                    &
-           (2*a1*a2)/(r12**2.-(a1-a2)**2.) +                    &
-            log((r12**2.-(a1+a2)**2.)/(r12**2.-(a1-a2)**2.))  )
+    Uatt = -hamaker_constant/(6.d00) * (                      &
+           (2*r1*r2)/(r12**2.-(r1+r2)**2.) +                  &
+           (2*r1*r2)/(r12**2.-(r1-r2)**2.) +                  &
+            log((r12**2.-(r1+r2)**2.)/(r12**2.-(r1-r2)**2.))  )
 
-    Urep = hamaker_constant*sigma**6./(37800.*r12)*  (                               &
-           ( r12**2.-7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12-a1-a2)**7.  &
-          +( r12**2.+7.*r12*(a1+a2)+6*(a1**2.+7.*a1*a2+a2**2.))/(r12+a1+a2)**7.  &
-          -( r12**2.+7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12+a1-a2)**7.  &
-          -( r12**2.-7.*r12*(a1-a2)+6*(a1**2.-7.*a1*a2+a2**2.))/(r12-a1+a2)**7.  )
-
-    Utot = Uatt + Urep
+    Urep = hamaker_constant*sigma**6./(37800.*r12)*  (                           &
+           ( r12**2.-7.*r12*(r1+r2)+6*(r1**2.+7.*r1*r2+r2**2.))/(r12-r1-r2)**7.  &
+          +( r12**2.+7.*r12*(r1+r2)+6*(r1**2.+7.*r1*r2+r2**2.))/(r12+r1+r2)**7.  &
+          -( r12**2.+7.*r12*(r1-r2)+6*(r1**2.-7.*r1*r2+r2**2.))/(r12+r1-r2)**7.  &
+          -( r12**2.-7.*r12*(r1-r2)+6*(r1**2.-7.*r1*r2+r2**2.))/(r12-r1+r2)**7.  )
 endif
-
-Uatt = Uatt/(boltz_const_Joule_K*Temp)
-Urep = Urep/(boltz_const_Joule_K*Temp)
-Utot = Utot/(boltz_const_Joule_K*Temp)
-
-if (Utot.gt.0.) then
-    Utot = 0.d0
-endif
-
-r12 = r12 * 1.e+10
 
 return
 !------------------------------------------------------------------------------------------------------!
