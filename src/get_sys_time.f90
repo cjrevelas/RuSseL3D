5c5957e01f1df6008355ad4b594e34d5a4182e47
cjrevelas <cjrevelas@gmail.com>
2021-12-06 21:01:42 +0200

    fix(get_sys_time): remove junk time.out.txt file

--- src/get_sys_time.f90
+++ src/get_sys_time.f90
@@ -10,6 +10,7 @@ status = system("date +%s > time.out.txt")
 open(unit=78, file = time)
 read(78, '(I20)')get_sys_time
 close(78)
+status = system("rm time.out.txt")
 return
 !----------------------------------------------------------------------------------------------------------!
 end function get_sys_time
