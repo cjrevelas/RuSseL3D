e2b5aeb9bf4cdd6807323a6054ed36613df7cd8d
cjrevelas <cjrevelas@gmail.com>
2021-12-06 15:51:33 +0200

    refactor(geometry): delete unused variables

--- src/mod/geometry.f90
+++ src/mod/geometry.f90
@@ -2,13 +2,12 @@ module geometry
 !--------------------------------------------------------------------!
 implicit none
 !--------------------------------------------------------------------!
-integer                              :: all_el
-integer                              :: nel, ndm, numnp, numel
+integer                              :: all_el, nel, ndm, numnp, numel
 integer, allocatable, dimension(:)   :: con_l2
-integer, allocatable, dimension(:,:) :: connectivity, con_l, ix
+integer, allocatable, dimension(:,:) :: ix
 
-logical, dimension(3,2)            :: is_dir_face
-logical, allocatable, dimension(:) :: elem_in_q0_face
+logical, dimension(3,2)              :: is_dir_face
+logical, allocatable, dimension(:)   :: elem_in_q0_face
 
 real(8), allocatable, dimension(:,:) :: xc
 !--------------------------------------------------------------------!
