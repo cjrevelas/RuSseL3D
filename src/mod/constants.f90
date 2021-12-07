3e3d8a59d12d585d9f7bc36633283e3832f2910f
cjrevelas <cjrevelas@gmail.com>
2021-12-07 09:08:54 +0200

    feat(constants): add some useful constants

--- src/mod/constants.f90
+++ src/mod/constants.f90
@@ -4,8 +4,10 @@ implicit none
 !--------------------------------------------------------------------!
 real(8), parameter :: boltz_const_Joule_molK = 8.3144598         !Boltzmann constant in J/mol/K.
 real(8), parameter :: boltz_const_Joule_K    = 1.38064852E-23    !Boltzmann constant in J/K.
-real(8), parameter :: avogadro_constant      = 6.02214085700E+23
+real(8), parameter :: n_avog                 = 6.02214085700E+23
 real(8), parameter :: pi                     = 3.14159265359
 real(8), parameter :: tol                    = 1.E-12
+real(8), parameter :: gr_cm3_to_kg_m3        = 1000              !kg/m^3
+real(8), parameter :: atm_to_pa              = 1.01325d05        !pa
 !--------------------------------------------------------------------!
 end module constants
