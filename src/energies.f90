f0f41c48c5e88bf53ce038130e04aaaa1da4cb3c
cjrevelas <cjrevelas@gmail.com>
2021-12-09 15:09:21 +0200

    refactor(energies): rename energy terms

--- src/energies.f90
+++ src/energies.f90
@@ -1,7 +1,7 @@
-subroutine energies(qmx_interp_mg, phi_total, part_func, num_gpoints, gpid, free_energy)
+subroutine energies(qmx_interp_mg, phi_total, wa, Ufield, part_func, num_gpoints, gpid, free_energy)
 !-------------------------------------------------------------------------------------------------!
 use eos,         only: eos_ff, eos_df_drho
-use parser_vars, only: ns_gr_conv, chainlen_mx, interf_area, rho_mol_bulk, temp
+use parser_vars, only: ns_gr_conv, chainlen_mx, interf_area, rho_mol_bulk, temp, beta
 use geometry,    only: numnp, box_lo, box_hi, xc
 use constants,   only: n_avog, boltz_const_Joule_molK, N_to_mN, A2_to_m2, A3_to_m3
 use iofiles,     only: energy_terms
@@ -13,41 +13,50 @@ integer, intent(in), dimension(num_gpoints) :: gpid
 integer                                     :: kk, gnode_id
 
 real(8), intent(in), dimension(ns_gr_conv+1,numnp) :: qmx_interp_mg
-real(8), intent(in), dimension(numnp)              :: phi_total
+real(8), intent(in), dimension(numnp)              :: phi_total, wa, Ufield
 real(8), intent(in)                                :: part_func
 real(8), intent(out)                               :: free_energy
 real(8)                                            :: Q=0.d0, vol=0.d0
-real(8)                                            :: term1=0.d0, term2=0.d0, term3=0.d0, term4=0.d0, term4_norm
-real(8)                                            :: r_ref = 0.4d0, r_gpoint =0.d0
+real(8)                                            :: E_eos_f=0.d0, E_eos_dfdrho=0.d0
+real(8)                                            :: E_field=0.d0, E_solid=0.d0
+real(8)                                            :: E_entropy_mx=0.d0, E_entropy_gr=0.d0
+real(8)                                            :: E_entropy_gr_normlz=0.d0
+real(8)                                            :: r_ref=0.4d0, r_gpoint=0.d0
 real(8), dimension(num_gpoints)                    :: term4_norm_gpoint, term4_gpoint
-real(8), dimension(numnp)                          :: dterm1, dterm2
+real(8), dimension(numnp)                          :: prof_eos_f, prof_eos_dfdrho
+real(8), dimension(numnp)                          :: prof_field, prof_solid
 !-------------------------------------------------------------------------------------------------!
-term4      = 0.d0
-term4_norm = 0.d0
-dterm1     = 0.d0
-dterm2     = 0.d0
+E_entropy_gr        = 0.d0
+E_entropy_gr_normlz = 0.d0
+
+prof_eos_f      = 0.d0
+prof_eos_dfdrho = 0.d0
+prof_field      = 0.d0
+prof_solid      = 0.d0
 
 do kk = 1, numnp
-   dterm1(kk) = eos_ff(phi_total(kk)) - eos_ff(1.d0)
-   dterm2(kk) = -phi_total(kk) * eos_df_drho(phi_total(kk)) + 1.d0*eos_df_drho(1.d0)
+   prof_eos_f(kk)      =  eos_ff(phi_total(kk)) - eos_ff(1.d0)
+   prof_eos_dfdrho(kk) = -phi_total(kk)*eos_df_drho(phi_total(kk)) + 1.d0*eos_df_drho(1.d0)
+   prof_field(kk)      = -phi_total(kk)*wa(kk)
+   prof_solid(kk)      =  phi_total(kk)*Ufield(kk)/beta
 enddo
 
-call spat_3d(dterm1, term1, Q, vol)
-call spat_3d(dterm2, term2, Q, vol)
+call spat_3d(prof_eos_f, E_eos_f, Q, vol)
+call spat_3d(prof_eos_dfdrho, E_eos_dfdrho, Q, vol)
+call spat_3d(prof_field, E_field, Q, vol)
+call spat_3d(prof_solid, E_solid, Q, vol)
 
 E_eos_f      = E_eos_f      * A3_to_m3
 E_eos_dfdrho = E_eos_dfdrho * A3_to_m3 * rho_mol_bulk * n_avog
 E_entropy_mx = rho_mol_bulk * A3_to_m3 * vol * boltz_const_Joule_molK * Temp * (1.d00 - part_func) / chainlen_mx
 
 do kk = 1, num_gpoints
-    gnode_id = gpid(kk)
-    r_gpoint = MIN(ABS(xc(3,gnode_id) - box_lo(3)), ABS(box_hi(3) - xc(3,gnode_id)))
-
-    term4_gpoint(kk) = - boltz_const_Joule_K * Temp * LOG(qmx_interp_mg(ns_gr_conv+1,gnode_id))
-    term4            = term4 + term4_gpoint(kk)
-
-    term4_norm_gpoint(kk) = - boltz_const_Joule_K * Temp * LOG(r_ref / r_gpoint)
-    term4_norm            = term4_norm + term4_norm_gpoint(kk)
+    gnode_id              =  gpid(kk)
+    r_gpoint              =  MIN(ABS(xc(3,gnode_id)-box_lo(3)), ABS(box_hi(3)-xc(3,gnode_id)))
+    term4_gpoint(kk)      = -LOG(qmx_interp_mg(ns_gr_conv+1,gnode_id)) / beta
+    term4_norm_gpoint(kk) = -LOG(r_ref/r_gpoint) / beta
+    E_entropy_gr          =  E_entropy_gr        + term4_gpoint(kk)
+    E_entropy_gr_normlz   =  E_entropy_gr_normlz + term4_norm_gpoint(kk)
 enddo
 
 E_eos_f             = E_eos_f             * N_to_mN / (interf_area*A2_to_m2)
@@ -56,11 +65,11 @@ E_entropy_mx        = E_entropy_mx        * N_to_mN / (interf_area*A2_to_m2)
 E_entropy_gr        = E_entropy_gr        * N_to_mN / (interf_area*A2_to_m2)
 E_entropy_gr_normlz = E_entropy_gr_normlz * N_to_mN / (interf_area*A2_to_m2)
 
-free_energy = term1 + term2 + term3 + term4 + term4_norm
+free_energy = E_eos_f + E_eos_dfdrho + E_entropy_mx + E_entropy_gr + E_entropy_gr_normlz
 
 open(unit=837, file = energy_terms)
 write(837,'(A14,3A20,A22,A21)')   "term1", "term2", "term3", "term4", "term4_norm", "free_energy"
-write(837,'(6(E19.9E2,1X))')   term1,   term2,   term3,   term4,   term4_norm,   free_energy
+write(837,'(6(E19.9E2,1X))')   E_eos_f,   E_eos_dfdrho,   E_entropy_mx,   E_entropy_gr,   E_entropy_gr_normlz,   free_energy
 
 if (num_gpoints.ne.0) then
     write(837,*)
