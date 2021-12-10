0f33e00ee1f5f33e807f1716c47ca5c849d8fd13
cjrevelas <cjrevelas@gmail.com>
2021-12-10 16:01:35 +0200

    feat: print warning when h99 reaches box boundaries

--- src/export_brush99.f90
+++ src/export_brush99.f90
@@ -4,7 +4,9 @@
 
 subroutine export_brush99(cell_of_np, num_gpoints, numnp, file_name, phia_gr, phia_gr_indiv, volnp, lbin, nbin)
 !-----------------------------------------------------------------------------------------------------------!
-use write_helper, only : adjl
+use write_helper, only: adjl
+use parser_vars,  only: iow, n_nanopart_faces, radius_np_eff
+use geometry,     only: box_len
 !-----------------------------------------------------------------------------------------------------------!
 implicit none
 !-----------------------------------------------------------------------------------------------------------!
@@ -18,7 +20,7 @@ real(8), intent(in)                                :: lbin
 real(8), intent(in), dimension(numnp)              :: phia_gr, volnp
 real(8), intent(out), dimension(numnp,num_gpoints) :: phia_gr_indiv
 real(8)                                            :: mass_cumulative, np_mass
-real(8)                                            :: mass_total, brush99_all
+real(8)                                            :: mass_total, brush99_all, boxSize
 real(8)                                            :: brush99_of_chain_ave, brush99_of_chain_std
 real(8), dimension(num_gpoints)                    :: brush99_of_chain
 real(8), allocatable, dimension(:)                 :: mass_layer
@@ -46,7 +48,7 @@ do jj = 1, num_gpoints
             exit
         endif
     enddo
-    write(120,'(I19,F19.9)') jj, brush99_of_chain(jj)
+    write(120,'(I8,2X,E16.9E3)') jj, brush99_of_chain(jj)
 enddo
 
 !compute average
@@ -73,17 +75,26 @@ enddo
 mass_cumulative = 0.d0
 do ii = 1, nbin
     mass_cumulative = mass_cumulative + mass_layer(ii)
-    !write(*,*)ii, mass_layer(ii), mass_cumulative
     if (mass_cumulative > mass_total*0.99d0) then
         brush99_all = lbin * (REAL(ii) - 0.5d0)
         exit
     endif
 enddo
 
-write(120,'(A19,F19.9)') "mean" , brush99_of_chain_ave
-write(120,'(A19,F19.9)') "stdev", brush99_of_chain_std
-write(120,'(A19,F19.9)') "all"  , brush99_all
+write(120,'(A8,2X,E16.9E3)') "mean" , brush99_of_chain_ave
+write(120,'(A8,2X,E16.9E3)') "stdev", brush99_of_chain_std
+write(120,'(A8,2X,E16.9E3)') "all"  , brush99_all
 
+if (n_nanopart_faces/=0) then
+    do ii = 1, n_nanopart_faces
+        boxSize = min(box_len(1), box_len(2), box_len(3))/2.d0 - radius_np_eff(ii)
+    enddo
+
+    if (brush99_all > (0.95d0 * boxSize)) then
+        write(iow,'(2X,A52,2(A13,E11.3E3))') "WARNING: Grafted chains reach the box boundaries! ->", "Brush_h99%:", brush99_all, ", Box size:", boxSize
+        write(6  ,'(2X,A52,2(A13,E11.3E3))') "WARNING: Grafted chains reach the box boundaries! ->", "Brush_h99%:", brush99_all, ", Box size:", boxSize
+    endif
+endif
 write(6,'(2X,A40)')adjl("****************************************",40)
 close(120)
 deallocate(mass_layer)
