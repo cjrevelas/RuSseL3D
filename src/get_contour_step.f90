a2df4d913b4d8cdca43a8a145dcc75ea3762089b
cjrevelas <cjrevelas@gmail.com>
2022-03-15 11:45:31 +0200

    fix(get_contour_step): ns_part1 is initialized out of if-block

--- src/get_contour_step.f90
+++ src/get_contour_step.f90
@@ -16,8 +16,9 @@ integer              :: ns_part1, ns_part2
 real(8), intent(in)  :: ds_ave, xs_crit, chainlen
 real(8)              :: ds_max
 !------------------------------------------------------------------------------------------------------!
+ns_part1 = 2 * NINT(0.5d0 * xs_crit / ds_ave)
+
 if (chainlen.ge.xs_crit) then
-    ns_part1 = 2 * NINT(0.5d0 * xs_crit / ds_ave)
     ds_max   = (xs_crit * (1.d0 - DCOS(pi * (DBLE(ns_part1+1)-1.d0) / (DBLE(ns_part1) * 2.d0)))) &
 &            - (xs_crit * (1.d0 - DCOS(pi * (DBLE(ns_part1)  -1.d0) / (DBLE(ns_part1) * 2.d0))))
     ns_part2 = 2 * NINT(0.5d0*(chainlen - xs_crit)/ds_max)
@@ -39,6 +40,8 @@ else
 &          - (xs_crit * (1.d0 - DCOS(pi * (DBLE(ns_ed)  -1.d0) / (DBLE(ns_part1) * 2.d0))))
     write(iow,'(3X,A40)')adjl("Critical point larger than chainlength.",40)
     write(6  ,'(3X,A40)')adjl("Critical point larger than chainlength.",40)
+    write(iow,'(3X,A40,F16.1)')adjl("Maximum contour step size:",40), ds_max
+    write(6  ,'(3X,A40,F16.1)')adjl("Maximum contour step size:",40), ds_max
     write(iow,'(3X,A40,I16)')adjl("Number of contour steps:",40), ns_ed
     write(6  ,'(3X,A40,I16)')adjl("Number of contour steps:",40), ns_ed
 endif
