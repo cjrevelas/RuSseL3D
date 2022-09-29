be7ff2f7737765d824123600deddf263484522af
cjrevelas <cjrevelas@gmail.com>
2022-09-29 14:04:05 +0300

    perf(periodic): restore removal of O(n2) loops from corners

--- src/FemPeriodicCorners.f90
+++ src/FemPeriodicCorners.f90
@@ -3,109 +3,93 @@ subroutine FemPeriodicCorners(elemcon)
 use fhash_module__ints_double
 use ints_module
 use kcw_mod,      only: F_m
-use geometry_mod, only: nodePairingXXhash, &
-                        nodePairingYYhash, &
-                        nodePairingZZhash
+use geometry_mod, only: nodePairingZZhash,        &
+                        nodePairingXXhashInverse, &
+                        nodePairingYYhashInverse
 !------------------------------------------------------------------------------------------------------!
 implicit none
 !------------------------------------------------------------------------------------------------------!
-type(fhash_type_iterator__ints_double) :: nodePairingXXit, nodePairingYYit, nodePairingZZit
-type(fhash_type_iterator__ints_double) :: nodePairingXXitAux, nodePairingYYitAux
-type(ints_type)                        :: nodePairingXXkey, nodePairingYYkey, nodePairingZZkey
-integer                                :: nodePairingXXvalue, nodePairingYYvalue, nodePairingZZvalue
+type(fhash_type_iterator__ints_double) :: nodePairingZZit
+type(ints_type)                        :: nodePairingZZhashKey
+type(ints_type)                        :: nodePairingXXhashInverseKey, nodePairingYYhashInverseKey
+integer                                :: nodePairingZZvalue
 
 type(fhash_type__ints_double), intent(inout) :: elemcon
-type(ints_type)                              :: elemcon_key
+type(ints_type)                              :: elemconKey
 
-integer :: sourceXX, destXX
-integer :: sourceYY, destYY
+logical :: success, successAux
+
+integer :: sourceXX
+integer :: sourceYY
 integer :: sourceZZ, destZZ
 integer :: sourceAux=0, destTriple=0
-integer :: ii, jj, kk, mm, nn
+integer :: ii, mm, nn
 !------------------------------------------------------------------------------------------------------!
-allocate(elemcon_key%ints(2))
+allocate(elemconKey%ints(2))
+allocate(nodePairingYYhashInverseKey%ints(1))
+allocate(nodePairingXXhashInverseKey%ints(1))
 
 call nodePairingZZit%begin(nodePairingZZhash)
 
 do ii = 1, nodePairingZZhash%key_count()
-  call nodePairingZZit%next(nodePairingZZkey, nodePairingZZvalue)
-  sourceZZ = nodePairingZZkey%ints(1) ! sourceZZ = 9 or 48
-  destZZ   = nodePairingZZvalue       ! destZZ   = 1 or 38
+  call nodePairingZZit%next(nodePairingZZhashKey, nodePairingZZvalue)
+  sourceZZ = nodePairingZZhashKey%ints(1) ! sourceZZ = 9 or 48
+  destZZ   = nodePairingZZvalue           ! destZZ   = 1 or 38
 
-  call nodePairingYYit%begin(nodePairingYYhash)
-  do jj = 1, nodePairingYYhash%key_count()
-    call nodePairingYYit%next(nodePairingYYkey, nodePairingYYvalue)
-    sourceYY = nodePairingYYkey%ints(1) ! sourceYY = 7 or 23
-    destYY   = nodePairingYYvalue       ! destYY   = 1 or 38
+  nodePairingYYhashInverseKey%ints(1) = destZZ
+  call nodePairingYYhashInverse%get(nodePairingYYhashInverseKey, sourceYY, success) ! sourceAux = 7 or 23
 
-    if (destZZ.eq.destYY) then
-      call nodePairingYYitAux%begin(nodePairingYYhash)
-      do kk = 1, nodePairingYYhash%key_count()
-        call  nodePairingYYitAux%next(nodePairingYYkey, nodePairingYYvalue)
-        if (nodePairingYYvalue.eq.sourceZZ) then
-          sourceAux = nodePairingYYkey%ints(1) ! sourceAux = 21, 31
-          exit
-        endif
-      enddo
+  if (success) then
+    nodePairingYYhashInverseKey%ints(1) = sourceZZ
+    call nodePairingYYhashInverse%get(nodePairingYYhashInverseKey, sourceAux) ! 21 or 31
 
-      elemcon_key%ints(1) = sourceZZ
-      elemcon_key%ints(2) = sourceAux
-      call elemcon%get(elemcon_key, mm)
+    elemconKey%ints(1) = sourceZZ  ! 9  or 48
+    elemconKey%ints(2) = sourceAux ! 21 or 31
+    call elemcon%get(elemconKey, mm)
 
-      elemcon_key%ints(1) = destZZ
-      elemcon_key%ints(2) = sourceYY
-      call elemcon%get(elemcon_key, nn)
+    elemconKey%ints(1) = destZZ   ! 1 or 38 = destYY
+    elemconKey%ints(2) = sourceYY ! 7 or 23
+    call elemcon%get(elemconKey, nn)
 
-      F_m%g(mm) = F_m%g(mm) + F_m%g(nn)
-      F_m%g(nn) = 0.0d0
+    F_m%g(mm) = F_m%g(mm) + F_m%g(nn)
+    F_m%g(nn) = 0.0d0
 
-      call nodePairingXXit%begin(nodePairingXXhash)
-      do kk = 1, nodePairingXXhash%key_count()
-        call nodePairingXXit%next(nodePairingXXkey, nodePairingXXvalue)
-        destXX = nodePairingXXvalue
-        if (destZZ.eq.destXX) destTriple = destXX
-      enddo
-    endif
-  enddo
+    nodePairingXXhashInverseKey%ints(1) = destZZ ! = destYY = 1 or 38
+    call nodePairingXXhashInverse%get(nodePairingXXhashInverseKey, sourceXX, successAux)
+    if (successAux) destTriple = destZZ ! 38
+  endif
 enddo
 
 call nodePairingZZit%begin(nodePairingZZhash)
 do ii = 1, nodePairingZZhash%key_count()
-  call nodePairingZZit%next(nodePairingZZkey, nodePairingZZvalue)
-  sourceZZ = nodePairingZZkey%ints(1) ! sourceZZ = 31
-  destZZ   = nodePairingZZvalue       ! destZZ   = 23
+  call nodePairingZZit%next(nodePairingZZhashKey, nodePairingZZvalue)
+  sourceZZ = nodePairingZZhashKey%ints(1) ! sourceZZ = 31
+  destZZ   = nodePairingZZvalue           ! destZZ   = 23
 
-  call nodePairingXXit%begin(nodePairingXXhash)
-  do jj = 1, nodePairingXXhash%key_count()
-    call nodePairingXXit%next(nodePairingXXkey, nodePairingXXvalue)
-    sourceXX = nodePairingXXkey%ints(1) ! sourceXX = 7
-    destXX   = nodePairingXXvalue       ! destXX   = 23
+  nodePairingXXhashInverseKey%ints(1) = destZZ
+  call nodePairingXXhashInverse%get(nodePairingXXhashInverseKey, sourceXX, success) ! sourceXX = 7
 
-    if ((destZZ.eq.destXX).AND.(destZZ.ne.destTriple)) then
-      call nodePairingXXitAux%begin(nodePairingXXhash)
-      do kk = 1, nodePairingXXhash%key_count()
-        call nodePairingXXitAux%next(nodePairingXXkey, nodePairingXXvalue)
-        if (nodePairingXXvalue.eq.sourceZZ) then
-          sourceAux = nodePairingXXkey%ints(1) ! sourceAux = 21
-          exit
-        endif
-      enddo
+  if (success.and.(destZZ.ne.destTriple)) then
+    nodePairingXXhashInverseKey%ints(1) = sourceZZ
+    call nodePairingXXhashInverse%get(nodePairingXXhashInverseKey, sourceAux) ! sourceAux = 21
 
-      elemcon_key%ints(1) = sourceZZ
-      elemcon_key%ints(2) = sourceAux
-      call elemcon%get(elemcon_key, mm)
+    elemconKey%ints(1) = sourceZZ   ! 31
+    elemconKey%ints(2) = sourceAux  ! 21
+    call elemcon%get(elemconKey, mm)
 
-      elemcon_key%ints(1) = destZZ
-      elemcon_key%ints(2) = sourceXX
-      call elemcon%get(elemcon_key, nn)
+    elemconKey%ints(1) = destZZ     ! 23
+    elemconKey%ints(2) = sourceXX   ! 7
+    call elemcon%get(elemconKey, nn)
 
-      F_m%g(mm) = F_m%g(mm) + F_m%g(nn)
-      F_m%g(nn) = 0.0d0
-    endif
-  enddo
+    F_m%g(mm) = F_m%g(mm) + F_m%g(nn)
+    F_m%g(nn) = 0.0d0
+  endif
 enddo
 
-deallocate(elemcon_key%ints)
+deallocate(elemconKey%ints)
+deallocate(nodePairingYYhashInverseKey%ints)
+deallocate(nodePairingXXhashInverseKey%ints)
+
 return
 !------------------------------------------------------------------------------------------------------!
 end subroutine FemPeriodicCorners
